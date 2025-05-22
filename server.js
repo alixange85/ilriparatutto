fc887459c2302afb55a0230c1a0959c171e656d8
alixange85 <angeloscud85@ilriparatutto.com>
2025-05-22 13:54:49 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Backend iLRiparaTutto attivo'));
+app.listen(PORT, () => console.log(`Server attivo sulla porta ${PORT}`));
