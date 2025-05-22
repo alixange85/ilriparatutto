e59fee2108be655fac102df0e0d9959807e8ea5f
alixange85 <angeloscud85@ilriparatutto.com>
2025-05-22 15:07:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const cors = require('cors'); // <--- aggiungi questo
 const app = express();
+
+app.use(cors()); // <--- e questo
+
 const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => res.send('Backend iLRiparaTutto attivo'));
+
 app.listen(PORT, () => console.log(`Server attivo sulla porta ${PORT}`));
